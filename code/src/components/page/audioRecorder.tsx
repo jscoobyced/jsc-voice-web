3cebce7203c5734d1981460a3a31515a925c8557
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2025-09-10 21:26:34 +0700

    Implement speech to text to speech

--- code/src/components/page/audioRecorder.tsx
+++ code/src/components/page/audioRecorder.tsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react'
 import AudioSocket from '../../service/audioWebSocket'
+import { playBuffer } from '../../service/playAudio'
 import Recorder from '../../service/recorder'
 
 const AudioRecorder: React.FC = () => {
   const [isRecording, setIsRecording] = useState(false)
+  const [message, setMessage] = useState('')
   const audioSocketService = new AudioSocket()
   const recorder = useRef(new Recorder())
 
+  const updateMessage = (data: any) => {
+    if (typeof data === 'string') {
+      setMessage(data)
+    } else {
+      setMessage('Response received')
+      const blob = new Blob([data], { type: 'audio/wav' })
+      playBuffer(blob)
+    }
+  }
+
   useEffect(() => {
-    audioSocketService.connect()
+    audioSocketService.connect(updateMessage)
     recorder.current.setCallback(audioSocketService.sendBlob)
   }, [])
 
@@ -30,6 +42,12 @@ const AudioRecorder: React.FC = () => {
       <button onClick={stopRecording} disabled={!isRecording}>
         Stop
       </button>
+      <div className="pt-4">
+        Status: {isRecording ? 'Recording...' : 'Idle'}
+      </div>
+      <div className="pt-4">
+        {message ? `Message received: ${message}` : ''}
+      </div>
     </div>
   )
 }
