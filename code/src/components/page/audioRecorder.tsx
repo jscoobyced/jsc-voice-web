cc2e0cca93f1d392f94553f6705f2b587585e8e3
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2025-09-13 11:18:27 +0700

    Complete first useable release.

--- code/src/components/page/audioRecorder.tsx
+++ code/src/components/page/audioRecorder.tsx
@@ -3,17 +3,24 @@ import AudioSocket from '../../service/audioWebSocket'
 import { playBuffer } from '../../service/playAudio'
 import Recorder from '../../service/recorder'
 
+interface StoryResponse {
+  type: string
+  content: string
+}
+
 const AudioRecorder: React.FC = () => {
   const [isRecording, setIsRecording] = useState(false)
-  const [message, setMessage] = useState('')
+  const [tellerMessage, setTellerMessage] = useState('')
+  const [userMessage, setUserMessage] = useState('')
   const audioSocketService = new AudioSocket()
   const recorder = useRef(new Recorder())
 
   const updateMessage = (data: any) => {
     if (typeof data === 'string') {
-      setMessage(data)
+      const response = JSON.parse(data) as StoryResponse
+      if (response.type === 'teller') setTellerMessage(response.content)
+      if (response.type === 'user') setUserMessage(response.content)
     } else {
-      setMessage('Response received')
       const blob = new Blob([data], { type: 'audio/wav' })
       playBuffer(blob)
     }
@@ -35,20 +42,37 @@ const AudioRecorder: React.FC = () => {
   }
 
   return (
-    <div>
-      <button onClick={startRecord} disabled={isRecording}>
-        Record
-      </button>
-      <button onClick={stopRecording} disabled={!isRecording}>
-        Stop
-      </button>
-      <div className="pt-4">
-        Status: {isRecording ? 'Recording...' : 'Idle'}
+    <>
+      <div className="text-center">
+        <button
+          className="p-10 m-5"
+          onClick={startRecord}
+          disabled={isRecording}
+        >
+          Record
+        </button>
+        <button
+          className="p-10 m-5"
+          onClick={stopRecording}
+          disabled={!isRecording}
+        >
+          Stop
+        </button>
+        <div className="pt-4">
+          Status: {isRecording ? 'Recording...' : 'Idle'}
+        </div>
       </div>
-      <div className="pt-4">
-        {message ? `Message received: ${message}` : ''}
+      <div className="flex justify-center items-center">
+        <pre className="pt-4 font-mono w-1/2 whitespace-normal bg-cyan-950 p-5">
+          {userMessage ? userMessage : ''}
+        </pre>
       </div>
-    </div>
+      <div className="flex justify-center items-center">
+        <pre className="pt-4 font-mono w-1/2 whitespace-pre-wrap bg-cyan-950 p-5">
+          {tellerMessage ? tellerMessage : ''}
+        </pre>
+      </div>
+    </>
   )
 }
 
